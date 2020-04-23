12df61bfe8d91a055436c0f24d2e5f962ec5d09f
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-04-23 11:57:16 +0100

    Edit Individual students

--- students_controller.rb
+++ students_controller.rb
@@ -30,7 +30,15 @@ post '/students' do
 end
 
 # edit
+get '/students/:id/edit' do 
+    @student = Student.find(params[:id])
+    erb(:edit)
+end
 
 # update
+post '/students/:id' do
+    Student.new(params).update
+    redirect to '/students'
+end
 
 # destroy
